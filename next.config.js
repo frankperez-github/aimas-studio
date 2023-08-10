ebfd317b9e90a1a83b210dd0e813fa7fbeb834ce
frankperez-github <fp848584@gmail.com>
2023-08-10 12:18:09 -0400

    blog section added

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    nextConfig,
+    env: {
+        GA_Tracking_ID: "G-FZCNBXVSG9"
+    }
+}
