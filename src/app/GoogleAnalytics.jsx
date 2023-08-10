da35e14f4ac822bc8a7c681e21ad0d1ef0a69208
frankperez-github <fp848584@gmail.com>
2023-08-10 11:42:34 -0400

    Google Analytics

--- src/app/GoogleAnalytics.jsx
+++ src/app/GoogleAnalytics.jsx
@@ -1,17 +1,17 @@
 'use client'
 import Script from "next/script"
-export default function GoogleAnalytics({GA_Trancking_ID})
+export default function GoogleAnalytics({GA_Tracking_ID})
 {
     return(
         <>
-            <Script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_Trancking_ID}`}></Script>
+            <Script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_Tracking_ID}`}></Script>
             <Script>
                 {`
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
 
-                    gtag('config', "${GA_Trancking_ID}");
+                    gtag('config', "${GA_Tracking_ID}");
                 
                 `}
             </Script>
--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <GoogleAnalytics GA_Trancking_ID={env.GA_Trancking_ID}/>
+      <GoogleAnalytics GA_Tracking_ID={env.GA_Tracking_ID}/>
       <SiteContextProvider>
         <body className={inter.className}>{children}</body>
       </SiteContextProvider>
